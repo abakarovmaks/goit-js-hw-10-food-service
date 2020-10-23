ee191ef5405d94133224997bb13ccb523f78de0f
abakarovmaks <abakarovmaks@gmail.com>
2020-10-23 22:24:26 +0300

    Добавляет функцию смены цвета фона

--- src/index.js
+++ src/index.js
@@ -12,4 +12,25 @@ function createList(menuItems) {
     return foodCards(menuItems);
 }
 
-// console.log(foodCards(menuItems));
+
+const bodyElem = document.querySelector('body');
+const inputElem = document.querySelector('#theme-switch-toggle');
+inputElem.addEventListener('change', clickOnInput);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+function clickOnInput(e) {
+    if (e.target.checked) {
+        bodyElem.classList.add(`${ Theme.DARK }`);
+        bodyElem.classList.remove(`${ Theme.LIGHT }`);
+        localStorage.setItem('Theme', `${ Theme.DARK }`);
+    }
+    else {
+        bodyElem.classList.add(`${ Theme.LIGHT }`);
+        bodyElem.classList.remove(`${ Theme.DARK }`);
+        localStorage.setItem('Theme', `${ Theme.LIGHT }`);
+    }
+}
